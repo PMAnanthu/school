fc1b33089f6306caa4401a90e62af3fa2f0553ce
Ananthu PM <ananthupmadhu@gmail.com>
2021-07-24 15:53:10 +0530

    add script

--- services/build.sh
+++ services/build.sh
@@ -1,19 +1,19 @@
 cd api-gateway-service
-gradlew.sh clean build buildDocker pushDocker
+gradlew clean build buildDocker pushDocker
 cd ..
 
 cd authentication-service
-gradlew.sh clean build buildDocker pushDocker
+gradlew clean build buildDocker pushDocker
 cd ..
 
 cd eureka-naming-server
-gradlew.sh clean build buildDocker pushDocker
+gradlew clean build buildDocker pushDocker
 cd ..
 
 cd hystrix-dashboard
-gradlew.sh clean build buildDocker pushDocker
+gradlew clean build buildDocker pushDocker
 cd ..
 
 cd notification-service
-gradlew.sh clean build buildDocker pushDocker
+gradlew clean build buildDocker pushDocker
 cd ..
