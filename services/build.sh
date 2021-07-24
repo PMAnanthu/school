7d71502ccb871726a2372cde06ec2e3bc38f3bbe
Ananthu PM <ananthupmadhu@gmail.com>
2021-07-24 15:58:59 +0530

    add script

--- services/build.sh
+++ services/build.sh
@@ -1,8 +1,6 @@
 
-cd api-gateway-service/
-gradlew  build buildDocker pushDocker
-cd ..
-authentication-service/gradlew  build buildDocker pushDocker
-eureka-naming-server/gradlew  build buildDocker pushDocker
-hystrix-dashboard/gradlew  build buildDocker pushDocker
-notification-service/gradlew  build buildDocker pushDocker
+./api-gateway-service/gradlew clean build buildDocker pushDocker
+./authentication-service/gradlew clean build buildDocker pushDocker
+./eureka-naming-server/gradlew clean build buildDocker pushDocker
+./hystrix-dashboard/gradlew clean build buildDocker pushDocker
+./notification-service/gradlew clean build buildDocker pushDocker
