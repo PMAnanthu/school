0f6ba71a5415e6040ad1a16c4d269831c42954a1
Ananthu PM <ananthupmadhu@gmail.com>
2021-07-25 16:49:26 +0530

    fix user bug

--- services/user-management-service/src/main/java/com/school/studentmanagementservice/service/UserMappingService.java
+++ services/user-management-service/src/main/java/com/school/studentmanagementservice/service/UserMappingService.java
@@ -1,9 +1,11 @@
 package com.school.studentmanagementservice.service;
 
+import com.school.studentmanagementservice.dto.Parent;
 import com.school.studentmanagementservice.dto.Staff;
 import com.school.studentmanagementservice.dto.Student;
 import com.school.studentmanagementservice.dto.UserMapping;
 import com.school.studentmanagementservice.http.dto.UserResponse;
+import com.school.studentmanagementservice.repo.IParentRepo;
 import com.school.studentmanagementservice.repo.IStaffRepo;
 import com.school.studentmanagementservice.repo.IStudentRepo;
 import com.school.studentmanagementservice.repo.IUserMappingRepo;
@@ -19,15 +21,16 @@ public class UserMappingService {
     private final IUserMappingRepo iUserMappingRepo;
     private final IStudentRepo iStudentRepo;
     private final IStaffRepo iStaffRepo;
+    private final IParentRepo parentRepo;
 
     public UserResponse findUser(String userId) {
         UserMapping userMapping = iUserMappingRepo.findByUserId(UUID.fromString(userId));
         if (userMapping != null) {
             UserResponse response = new UserResponse();
-            response.setSchool(userMapping.getSchool().toString());
             response.setUserType(userMapping.getUserType().toString());
             switch (userMapping.getUserType()) {
                 case STUDENT:
+                    response.setSchool(userMapping.getSchool().toString());
                     Student student = iStudentRepo.findNameByUserId(UUID.fromString(userId));
                     response.setFirstName(student.getFirstName() );
                     response.setMiddleName(student.getMiddleName());
@@ -36,11 +39,19 @@ public class UserMappingService {
                 case TEACHER:
                 case FINANCE_MANGER:
                 case ACADEMIC_MANGER:
+                    response.setSchool(userMapping.getSchool().toString());
                     Staff staff=iStaffRepo.findByUserId(UUID.fromString(userId));
                     response.setFirstName(staff.getFirstName() );
                     response.setMiddleName(staff.getMiddleName());
                     response.setLastName(staff.getLastName());
                     break;
+                case PARENT:
+                    Parent parent=parentRepo.findByUserId(UUID.fromString(userId));
+                    response.setFirstName(parent.getFirstName() );
+                    response.setMiddleName(parent.getMiddleName());
+                    response.setLastName(parent.getLastName());
+                    break;
+
             }
             return response;
         }
